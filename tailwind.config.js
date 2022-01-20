3325257cb5f8fef481dcd10a9058923507f43721
delphinemillet <del.millet@gmail.com>
2022-01-20 14:31:42 +0100

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,11 @@ const configuration = require('@backmarket/design-system/tailwind.config')
 module.exports = {
   ...configuration,
   purge: [
-    './app/components/**/*.vue',
-    './app/layouts/**/*.vue',
-    './modules/**/*.vue',
-
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
     // Make sure not to purge classes used by design system components.
     './node_modules/@backmarket/design-system/dist/esm/**/*',
   ],
